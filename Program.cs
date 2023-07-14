599d13e6c0237088d0808c1dba10cc3f9c369918
singhvishal14 <60248427+singhvishal14@users.noreply.github.com>
2023-07-14 15:53:02 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -26,15 +26,19 @@ namespace DAY05_Assishnment_07
             {
                     Console.WriteLine("Not Contain");
             }
+            Console.WriteLine("Entered the fruit Name you want to Insert: ");
+            string FruitInsert=Console.ReadLine();
             Console.WriteLine("After Inserting: ");
-            fruitsList.Insert(1, "fig");
+            fruitsList.Insert(1, FruitInsert);
             Console.WriteLine("The Number of Fruits are in fruitsList is: " + fruitsList.Count);
             foreach (string str in fruitsList)
             {
                 Console.WriteLine(str);
             }
+            Console.WriteLine("Entered the fruit Name you want to Remove: ");
+            string FruitRemove=Console.ReadLine();
             Console.WriteLine("After Removing: ");
-            fruitsList.Remove("banana");
+            fruitsList.Remove(FruitRemove);
             Console.WriteLine("The Number of Fruits are in fruitsList is: " + fruitsList.Count);
             foreach (string str in fruitsList)
             {
